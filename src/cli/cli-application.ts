fbda472622060e4a530181550575ab1fd1f74109
Nata <erbet@mail.ru>
2024-01-19 12:08:35 +0300

    feat: 1.7 add prossesCommand

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,27 +1,44 @@
 // import { VersionCommand } from './commands/version.command.js';
 // import { HelpCommand  } from './commands/help.command.js';
 import { CommnadInterface } from  './commands/command.interface.js';
+import { CommandParser } from './commands/command.parser.js'
 
 type CommandCollection = Record<string, CommnadInterface>
 
 class CLIApplication {
-  private commads: CommandCollection =  {}
+  private commands: CommandCollection =  {}
 
   constructor(
-
+    private readonly defaultCommand = '--help',
   ) {}
+
+public getCommand(commandName: string): CommnadInterface {
+  return this.commands[commandName] ?? this.getDefaultCommand;
+}
+
+public getDefaultCommand(): CommnadInterface | never {
+  if(!this.commands[this.defaultCommand]){
+    throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+  }
+  return this.commands[this.defaultCommand];
+}
 
  public registerCommand(commandList: CommnadInterface[]): void {
   commandList.forEach((commandItem) => {
     if(Object.hasOwn(commandList, commandItem.getName())) {
       throw new Error(`Command ${commandItem.getName()} is already registered`);
     }
-    this.commads[commandItem.getName()] = commandItem;
+    this.commands[commandItem.getName()] = commandItem;
   })
  }
 
-  public parsedInput () {
+  public prossesCommand (arg: string[]) {
+    const parsedCommand = CommandParser.parse(arg);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArg = parsedCommand[commandName] ?? [];
 
+    command.execute(...commandArg);
   }
 }
 
