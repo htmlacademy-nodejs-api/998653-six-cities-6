345eaa99d0ced1b5fbe3770b67069b9267ae0828
Nata <erbet@mail.ru>
2024-01-26 20:53:48 +0300

    feat: add genetate-command into main.cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,3 +2,4 @@ export * from './cli-application.js';
 export * from './commands/command.parser.js';
 export * from './commands/help.command.js';
 export * from './commands/version.command.js';
+export * from './commands/generation.command.js';
--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,11 +1,12 @@
-import { CLIApplication, HelpCommand, VersionCommand } from './cli/index.js';
+import { CLIApplication, HelpCommand, VersionCommand, GenerateCommand } from './cli/index.js';
 
 function bootstrap() {
   const cliApp = new CLIApplication();
 
   cliApp.registerCommands([
     new HelpCommand(),
-    new VersionCommand()
+    new VersionCommand(),
+    new GenerateCommand()
   ]);
 
   cliApp.prossesCommand(process.argv);
