a57af90b77f0fb3016ab3b1458f7b7ef155265f9
Nata <erbet@mail.ru>
2024-01-27 12:40:22 +0300

    reafactor import-command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,12 +1,35 @@
 import { CommnadInterface } from './command.interface.js';
+import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { CreateOffer, getErrorMessage } from '../../shared/helpers/index.js';
 
 class ImportCommand implements CommnadInterface {
   public getName(): string {
     return '--import';
   }
 
-  public execute(..._params: string[]): void {
+  private onImportedLine(line: string) {
+    const offer = CreateOffer(line);
+    console.info(offer);
+  }
 
+  private onCompliteEnd(count: number) {
+    console.info(`${count} rows imported.`);
+
+  }
+
+  public async execute(..._params: string[]): Promise<void> {
+    const [ filename ] = _params;
+    const fileReader = new TSVFileReader(filename);
+
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompliteEnd);
+
+    try {
+      await fileReader.read();
+    } catch (error) {
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(getErrorMessage(error));
+    }
   }
 }
 
