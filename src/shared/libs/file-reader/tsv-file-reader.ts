2e732ada79cd645fca7f6bc5b8c4a03ffcae6c11
Nata <erbet@mail.ru>
2024-01-27 10:44:11 +0300

    feat: add method rear into tsv-file-file-reader

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,6 +1,10 @@
+
 import EventEmitter from 'node:events';
 import { FileReader } from './file-reader.interface.js';
+import { createReadStream } from 'node:fs';
 
+
+const CHUNK_SIZE = 16384; // 16KB
 
 class TSVFileReader extends EventEmitter implements FileReader {
   constructor(
@@ -9,9 +13,29 @@ class TSVFileReader extends EventEmitter implements FileReader {
     super();
   }
 
-  public read() {
+  public async read() {
+    const readStream = createReadStream(this.filename,{
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf8',
+    });
 
-  };
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
+    }
+    this.emit('end', importedRowCount);
+  }
 }
 
 export { TSVFileReader};
