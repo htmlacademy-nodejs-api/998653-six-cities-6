655b5c044d46c839382d081223029151604aa6a9
Nata <erbet@mail.ru>
2024-02-01 18:47:21 +0300

    test: success Get value from env $PORT: 5000

--- src/rest/rest-application.ts
+++ src/rest/rest-application.ts
@@ -1,11 +1,14 @@
+import { Config } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
   constructor(
-    private logger: Logger
+    private logger: Logger,
+    private config: Config
   ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
