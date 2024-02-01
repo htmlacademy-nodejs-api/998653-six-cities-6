b506c963ece9f8c53240a2c3aefd2118fc97ce03
Nata <erbet@mail.ru>
2024-02-01 18:10:25 +0300

    add  dotenv into  RestCong

--- src/shared/libs/config/rest-config.ts
+++ src/shared/libs/config/rest-config.ts
@@ -1,11 +1,25 @@
+import { DotenvParseOutput, config } from 'dotenv';
 import { Config } from './config.interface.js';
+import { Logger } from '../logger/index.js';
 
 
 export class RestConfig implements Config {
   private readonly config: NodeJS.ProcessEnv;
 
-  constructor(){}
-  get(key: string): string | undefined {
+  constructor(
+    private readonly logger: Logger
+  ){
+    const parsedOut = config();
+
+    if(parsedOut.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOut.parsed;
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get(key: string): string | undefined {
     return this.config[key];
   }
 }
