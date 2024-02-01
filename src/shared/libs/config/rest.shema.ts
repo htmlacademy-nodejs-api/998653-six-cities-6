5dfad9804fb5c9f565984d7d63aa250cdeb9bc2e
Nata <erbet@mail.ru>
2024-02-01 19:49:31 +0300

    add env into rest-application

--- src/shared/libs/config/rest.shema.ts
+++ src/shared/libs/config/rest.shema.ts
@@ -1,15 +1,33 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type RestShema ={
-  PORT :number
+  PORT :number,
+  SALT: string,
+  DB_HOST:string
 }
 
 export const configRestShema = convict<RestShema>({
   PORT: {
     env: 'PORT',
     default: 4000,
-    format: 'port',
+    format: String,
     doc: 'Port for incoming connections'
-  }
-});
+  },
+  SALT: {
+    env: 'PORT',
+    default: null,
+    format: 'port',
+    doc: 'Salt for password hash'
+  },
+  DB_HOST: {
+    env: 'DB_HOST',
+    default: '127.0.0.1',
+    format: 'ipaddress',
+    doc: 'IP address of the database server (MongoDB)'
+  },
+},
+);
 
