7ceb7b2daab02ff59ba5988502150d84a9e12887
Nata <erbet@mail.ru>
2024-02-01 15:11:10 +0300

    main-rest

---
+++ src/main.rest.ts
@@ -0,0 +1,13 @@
+import { RestApplication } from './rest/index.js';
+import { PinoLogger } from './shared/libs/logger/index.js';
+
+async function bootstrap() {
+  const logger = new PinoLogger();
+
+  const application = new RestApplication(logger);
+  application.init();
+}
+
+bootstrap();
+
+
