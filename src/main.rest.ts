2d90224ceb5e173b7ae787d5a605baaede074681
Nata <erbet@mail.ru>
2024-02-04 14:29:27 +0300

    refresh main.rest

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { Component } from './shared/types/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 import { RestApplication } from './rest/index.js';
 import { PinoLogger, Logger } from './shared/libs/logger/index.js';
@@ -12,6 +13,7 @@ async function bootstrap() {
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestShema>>(Component.Config).to(RestConfig).inSingletonScope();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inRequestScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   application.init();
