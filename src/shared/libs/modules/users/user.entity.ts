ca3f03798600e9f942e682f8c3ee87732f91554c
Nata <erbet@mail.ru>
2024-02-04 16:07:20 +0300

    add class UserEntit

--- src/shared/libs/modules/users/user.entity.ts
+++ src/shared/libs/modules/users/user.entity.ts
@@ -1,5 +1,8 @@
 import { TUser } from '../../../types/index.js';
 
 export class UserEntity {
-  
+  public email: string;
+  public avatarPath: string;
+  public firstname: string;
+  public lastname: string;
 }
