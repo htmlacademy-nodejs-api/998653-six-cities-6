4b4813d4b4c7cffb08f82c0041792a3a00948826
Nata <erbet@mail.ru>
2024-02-04 14:22:36 +0300

    refresh  Component

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -1,6 +1,7 @@
 export const Component = {
   RestApplication: Symbol.for('RestApplication'),
   Config: Symbol.for('Config'),
-  Logger: Symbol.for('Logger')
+  Logger: Symbol.for('Logger'),
+  DatabaseClient: Symbol.for('DatabaseClient'),
 };
 
