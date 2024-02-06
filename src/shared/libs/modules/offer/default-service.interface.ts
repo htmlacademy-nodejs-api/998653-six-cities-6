19430e7523e89e71af0109872251552d5c2a0a2b
Nata <erbet@mail.ru>
2024-02-06 09:09:21 +0300

    add default-service.interface

--- src/shared/libs/modules/offer/default-service.interface.ts
+++ src/shared/libs/modules/offer/default-service.interface.ts
@@ -1,6 +1,27 @@
-import { CreateUserDto } from './dto/create-offer.dto.js';
-import { OfferEntity } from './index.js'
+import { inject, injectable } from 'inversify';
+import { DocumentType, types } from '@typegoose/typegoose';
+import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { OfferEntity, OfferService, OfferModel } from './index.js';
+import { Logger } from '../../logger/index.js';
+import { Component } from '../../../types/index.js';
 
-export class DefaultOfferService {
+@injectable()
+export class DefaultOfferService implements OfferService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.OfferModel) private readonly offerModel: types.ModelType<OfferEntity>
+  ){}
+
+
+  public async create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>> {
+    const result = this.offerModel.create(dto);
+    this.logger.info(`New offer created: ${dto.name}`);
+
+    return result;
+  }
+
+  findOrCreate(offerId: string): Promise<DocumentType<OfferEntity>> | null{
+    return this.offerModel.findById(offerId).exec();
+  }
 
 }
--- src/shared/libs/modules/offer/offer-service.interface.ts
+++ src/shared/libs/modules/offer/offer-service.interface.ts
@@ -2,6 +2,6 @@ import { CreateOfferDto, OfferEntity } from './index.js';
 import { DocumentType } from '@typegoose/typegoose';
 
 export interface OfferService {
-  create(dto: CreateOfferDto, salt: string):Promise<DocumentType<OfferEntity>>
-  findOrCreate(dto: CreateOfferDto, salt: string): Promise<DocumentType<OfferEntity>>;
+  create(dto: CreateOfferDto):Promise<DocumentType<OfferEntity>>
+  findOrCreate(offerId: string): Promise<DocumentType<OfferEntity>> | null;
 }
