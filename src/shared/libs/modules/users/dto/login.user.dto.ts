63a96d7df2f40aa754099626333455ba2338db75
Nata <erbet@mail.ru>
2024-03-02 13:53:03 +0300

    add login-user.message

--- src/shared/libs/modules/users/dto/login.user.dto.ts
+++ src/shared/libs/modules/users/dto/login.user.dto.ts
@@ -1,4 +1,8 @@
+import {IsEmail, IsString, Length} from 'class-validator';
+import {LoginUserMessage} from './login-user.messages.js';
+
 export class LoginUserDto {
+  
   public email: string;
   public password: string;
 }
