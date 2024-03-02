41829ffc568731ac760698369387307b9790a3a6
Nata <erbet@mail.ru>
2024-03-02 12:12:15 +0300

    add validate-dto.middleware

---
+++ src/shared/libs/rest/middleware/validate-dto.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { Middleware } from './middleware.interface.js';
+import { validate } from 'class-validator';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { StatusCodes } from 'http-status-codes';
+
+export class ValidateDtoMMiddleware implements Middleware{
+  constructor(
+    private dto: ClassConstructor<object>
+  ) {}
+
+  public async execute({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+    const dtoInstance = plainToInstance(this.dto, body);
+    const errors = await validate(dtoInstance);
+
+    if(errors.length > 0) {
+      res.status(StatusCodes.BAD_REQUEST).send(errors);
+      return;
+    }
+
+    next();
+  }
+}
