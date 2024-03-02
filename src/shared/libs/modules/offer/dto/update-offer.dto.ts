18ee21ed20c87adece5d8721208bad11cfeac4da
Nata <erbet@mail.ru>
2024-03-02 19:38:29 +0300

    add @IsOptional to update-offer-dto

--- src/shared/libs/modules/offer/dto/update-offer.dto.ts
+++ src/shared/libs/modules/offer/dto/update-offer.dto.ts
@@ -6,6 +6,7 @@ import {
   IsBoolean,
   IsEnum,
   IsInt,
+  IsOptional,
   IsUrl,
   Matches,
   Max,
@@ -17,51 +18,63 @@ import {
 import { CreateOfferValidationMessage } from '../dto/index.js';
 
 export class UpdateOfferDto {
+  @IsOptional()
   @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
   @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
   public name?: string;
 
+  @IsOptional()
   @MinLength(20, { message: CreateOfferValidationMessage.description.minLength })
   @MaxLength(1024, { message: CreateOfferValidationMessage.description.maxLength })
   public description?: string;
 
+  @IsOptional()
   @IsEnum(CityType, { message: CreateOfferValidationMessage.city.invalidFormat })
   public city?: CityType;
 
+  @IsOptional()
   @IsUrl({}, { message: CreateOfferValidationMessage.imagePreview.isUrl })
   @Matches(/\.(jpg|png)(\?.*)?$/i, { message: CreateOfferValidationMessage.imagePreview.matches })
   public prevImg?: string;
 
+  @IsOptional()
   @IsArray({ message: CreateOfferValidationMessage.photos.invalidFormat})
   @ArrayMinSize(6, { message: CreateOfferValidationMessage.photos.ArrayMinSize })
   @ArrayMaxSize(6, { message: CreateOfferValidationMessage.photos.ArrayMaxSize })
   public photos?: string[];
 
+  @IsOptional()
   @IsBoolean({ message: CreateOfferValidationMessage.isPremium.invalidFormat })
   public isPremium?: boolean;
 
+  @IsOptional()
   @IsEnum(FlatType, { message: CreateOfferValidationMessage.houseType.invalidFormat })
   public flat?: FlatType;
 
+  @IsOptional()
   @IsArray({message: CreateOfferValidationMessage.houseType.invalidFormat})
   public inside?: InsideType;
 
+  @IsOptional()
   @IsInt({ message: CreateOfferValidationMessage.countRooms.invalidFormat })
   @Min(1, { message: CreateOfferValidationMessage.countRooms.minValue })
   @Max(8, { message: CreateOfferValidationMessage.countRooms.maxValue })
   public rooms?: number;
 
+  @IsOptional()
   @IsInt({ message: CreateOfferValidationMessage.countGuests.invalidFormat })
   @Min(1, { message: CreateOfferValidationMessage.countGuests.minValue })
   @Max(10, { message: CreateOfferValidationMessage.countGuests.maxValue })
   public adult?: number;
 
+  @IsOptional()
   @IsInt({ message: CreateOfferValidationMessage.rentPrice.invalidFormat })
   @Min(100, { message: CreateOfferValidationMessage.rentPrice.minValue })
   @Max(100000, { message: CreateOfferValidationMessage.rentPrice.maxValue })
   public price?: number;
 
+  @IsOptional()
   @ValidateNested()
-  public coords: Location;
+  public coords?: Location;
 }
 
