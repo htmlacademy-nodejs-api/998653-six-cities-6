d064e0f9949012ced1f5282e56547ed6bac74eac
Nata <erbet@mail.ru>
2024-03-09 17:34:28 +0300

    add class validation.error

--- src/shared/libs/rest/types/index.ts
+++ src/shared/libs/rest/types/index.ts
@@ -5,4 +5,5 @@ export * from './request.params.type.js';
 export * from './param-offer-id.type.js';
 export * from './param-user-id.type.js';
 export * from './param-city-name.type.js';
+export * from './validation-error-field.type.js';
 
